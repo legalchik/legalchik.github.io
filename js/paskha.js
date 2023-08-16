399a9f340dc3cb0f327b3c2bb861d67adc912983
legalchik <93884665+legalchik@users.noreply.github.com>
2023-08-16 05:22:34 +0300

    Create paskha.js

---
+++ js/paskha.js
@@ -0,0 +1,4 @@
+console.warn("По отрудничеству в telezhku");
+console.group("Пасхалка");
+console.log("ха, а пасхалки нет, ибо я не знаю как их тут писать, так что press F");
+console.error("https://legalchik.github.io/cream");
